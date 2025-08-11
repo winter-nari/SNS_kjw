8de26ff42db96048a2813b60f8ba311283ae68c0
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-08-11 20:14:25 +0900

    feat: add MemberService interface with getMember method

---
+++ src/main/java/com/winternari/sns_project/domain/user/service/MemberService.java
@@ -0,0 +1,10 @@
+package com.winternari.sns_project.domain.user.service;
+
+import com.winternari.sns_project.domain.user.dto.response.MemberResponse;
+
+import java.util.UUID;
+
+public interface MemberService {
+
+    public MemberResponse getMember();
+}
