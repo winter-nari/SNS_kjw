556308c42d688f350ca4b05297e00b4ae6eecec4
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-08-11 20:14:55 +0900

    feat: add OauthRepository with custom queries for counting followers and following

--- src/main/java/com/winternari/sns_project/domain/user/repository/OauthRepository.java
+++ src/main/java/com/winternari/sns_project/domain/user/repository/OauthRepository.java
@@ -3,6 +3,8 @@ package com.winternari.sns_project.domain.user.repository;
 
 import com.winternari.sns_project.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,5 +14,11 @@ import java.util.UUID;
 public interface OauthRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByEmail(String email);
     Optional<UserEntity> findByProfile_Nickname(String nickname);
+
+    @Query("SELECT COUNT(f) FROM FollowEntity f WHERE f.following.id = :userId")
+    int countFollowers(@Param("userId") UUID userId);
+
+    @Query("SELECT COUNT(f) FROM FollowEntity f WHERE f.follower.id = :userId")
+    int countFollowing(@Param("userId") UUID userId);
 
 }
